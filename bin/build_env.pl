e6cb48c05cdf20aff21426b041e1ed70e1083fd6
Robert McLay <mclay@zaniahgroup.com>
2011-10-05 13:35:50 -0500

    initial checkin

---
+++ bin/build_env.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl 
+#---------------------------------------------------------
+# Derive the user environment and strip out a 
+# couple of key elements (to pass along to MVAPICH later).
+#
+# ks 5/3/06
+#
+# Now includes string size for calling routine.
+# Size is returned as first keypair with TACC_ENVLEN key.
+# See loop to remove environment variables with NULL.
+#
+# km 5/4/06
+#
+# Removed module, include, and manpath information.
+# Included method to remove variables of any specific prefix.
+# e.g. Compiler variables, ICC_, IFC_ and IDB_.
+#
+# km 8/31/06
+#
+#---------------------------------------------------------
+
+$varenv = '';
+@env_exclude_list = qw(PWD PS1 HOME SSH_CLIENT SSH_CONNECTION HOST TERM SSH_AUTH_SOCK SSH_TTY SHLVL LSB_MCPU_HOSTS LSB_HOSTS LSF_PM_HOSTIDS LSB_EEXEC_REAL_GID LSB_JOBRES_PID INFOPATH _LMFILES_ LOADEDMODULES LOCAL_MODULES_DIR MODULEPATH _MODULESBEGINENV_ MODULESHOME  MODULE_VERSION MODULE_VERSION_STACK INCLUDE MANPATH  LESS_TERMCAP_mb LESS_TERMCAP_md LESS_TERMCAP_me LESS_TERMCAP_se LESS_TERMCAP_so LESS_TERMCAP_ue LESS_TERMCAP_us ARCHIVER REMOTEHOST XTERM_VERSION);
+#$env_begin_with = '(^TACC_|^ICC_|^IFC_|^IDDB_|^CVS)';
+ $env_begin_with =       '( ^ICC_|^IFC_|^IDDB_)';
+
+# TACC change: propogate entire user environment. (1/9/04)
+
+my %exclude;
+foreach (@env_exclude_list) { $exclude{$_} = 1; }
+
+foreach (keys %ENV) {
+#                     To remove env vars set to NULL vvvvvvvvvvvvvvvvvvv
+#   unless ($exclude{$_} || $ENV{$_} =~ m/[ \n\t;*]/ || $ENV{$_} =~ /^$/        ) { $varenv .= " $_=$ENV{$_} "; }
+#   unless ($exclude{$_} || $ENV{$_} =~ m/[ \n\t;*]/                            ) { $varenv .= " $_=$ENV{$_} "; }
+    unless ($exclude{$_} || $_ =~  /$env_begin_with/ || $ENV{$_} =~ m/[ \n\t;*]/) { $varenv .= " $_=$ENV{$_} ";}
+}
+
+# Report length of environment string, in the reported environment 
+# with the TACC_ENVLEN variable.
+
+$base_size = length($varenv);
+$num_size  = length($base_size);
+$add_size  = $base_size + 12 + $num_size;
+$varenv  = "TACC_ENVLEN=$add_size" . $varenv;
+
+print "$varenv";
---
+++ bin/getmode.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#---------------------------------------------------------
+# Hopefully a kinder, gentler getmode()
+# Trying to keep as light-weight as possible as we
+# might get called in parallel.
+#
+# Primary launch types are currently defined as:
+# (1) mvapich1_ssh
+# (2) mvapich2_ssh
+# (3) openmpi_ssh (same for all version)
+# 
+# Note that we have kicked mpd to the curb...
+#---------------------------------------------------------
+
+# Delineate specific stacks that are supported por favor.
+
+if [ -n "$TACC_MPI_GETMODE" ]; then
+  echo "$TACC_MPI_GETMODE"
+else
+  case $MPICH_HOME in 
+      /opt/apps/*/mvapich/1.0.1)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/mvapich/1.0.1c)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/mvapich2/1.2)
+  	builtin echo "mvapich2_ssh";;
+      /opt/apps/*/openmpi/1.3*)
+  	builtin echo "openmpi_1.3_ssh";;
+      /opt/apps/*/openmpi/*)
+  	builtin echo "openmpi_ssh";;
+      /opt/apps/*/mvapich/1.0)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/mvapich-hybrid/*)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/hecura/*)
+  	builtin echo "mvapich2_ssh";;
+      /opt/apps/*/hecura-debug/*)
+  	builtin echo "mvapich2_ssh";;
+      /opt/apps/*/mvapich2/1.0)
+  	builtin echo "mvapich2_mpd";;
+      /opt/apps/*/mvapich-devel/*)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/mvapich/0.9.9)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/openmpi/1.2.4)
+  	builtin echo "openmpi_ssh";;
+      /opt/apps/*/mvapich-old/*)
+  	builtin echo "mvapich1_ssh";;
+      /opt/apps/*/mvapich2-debug/*)
+  	builtin echo "mvapich2_ssh";;
+      /opt/apps/*/mvapich-new/*)
+  	builtin echo "mvapich1_ssh";;
+      *)
+  	builtin echo "UNKNOWN";;
+  esac
+fi
+# if env | grep MPICH_HOME | grep -q mvapich2-devel; then
+#   builtin echo "mvapich2_ssh"
+# elif env | grep MPICH_HOME | grep -q mvapich2; then
+#   builtin echo "mvapich2_mpd"
+# elif env | grep MPICH_HOME | grep -q "openmpi/1.3"; then
+#   builtin echo "openmpi_1.3_ssh"
+# elif env | grep MPICH_HOME | grep -q openmpi; then
+#   builtin echo "openmpi_ssh"
+# elif env | grep MPICH_HOME | grep -q mvapich-devel; then
+#   builtin echo "mvapich1_devel_ssh"
+# elif env | grep MPICH_HOME | grep -q mvapich; then
+#   builtin echo "mvapich1_ssh"
+# else
+#   builtin echo "mvapich1_ssh"
+# fi
+
