5e42da970f436c9e36778c4041c926e471f1224b
Robert McLay <mclay@zaniahgroup.com>
2013-04-03 15:37:26 -0500

    support for multiple Rmap Dirs

--- bin/readRMap.lua
+++ bin/readRMap.lua
@@ -1,8 +1,7 @@
 require("strict")
 require("fileOps")
+require("string_split")
 local Dbg = require("Dbg")
-
-reverseMapT = {}
 
 function readRMap(reverseMapD)
 
@@ -10,21 +9,31 @@ function readRMap(reverseMapD)
    dbg.start("readRMap(",reverseMapD,")")
    -- open reverseMap file and read it in.
 
-   local reverseMapFn = pathJoin(reverseMapD,"reverseMapT.lua")
-   local rmF          = io.open(reverseMapFn,"r")
-   if (not rmF) then
-      reverseMapFn = pathJoin(reverseMapD,"reverseMapT.old.lua")
-      rmF          = io.open(reverseMapFn,"r")
-   end
+   local reverseMapT = {}
 
-   if (rmF) then
-      local whole  = rmF:read("*all")
-      rmF:close()
-      local func, msg = loadstring(whole)
-      if (func) then
-         func()
-      else
-         dbg.print("Problem with reverse map: ",msg,"\n")
+   for dir in reverseMapD:split(":") do
+
+      local reverseMapFn = pathJoin(reverseMapD,"reverseMapT.lua")
+      local rmF          = io.open(reverseMapFn,"r")
+      if (not rmF) then
+         reverseMapFn = pathJoin(reverseMapD,"reverseMapT.old.lua")
+         rmF          = io.open(reverseMapFn,"r")
+      end
+      
+      if (rmF) then
+         local whole  = rmF:read("*all")
+         rmF:close()
+         local func, msg = loadstring(whole)
+         if (func) then
+            func()
+         else
+            dbg.print("Problem with reverse map: ",msg,"\n")
+         end
+      end
+
+      local rmapT = _G.reverseMapT
+      for k, v in pairs(rmapT) do
+         reverseMapT[k] = v
       end
    end
 
